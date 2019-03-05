bcc4a9f2a7be7a5191862cfcb6c64762bce69dc1
Brian C Williams <brian@endgame.com>
2019-03-05 14:22:24 -0500

    Update question

--- hello_world_no_world/import_solution.py
+++ hello_world_no_world/import_solution.py
@@ -1,4 +1,6 @@
-"""Print `Hello World` without using the word 'world'"""
+"""Print `Hello World` without using the word 'world'
+Note: captialization and punctuation are important. "hello world", "Hello World!" and "HELLO WORLD" aren't valid
+"""
 import io
 from contextlib import redirect_stdout
 
@@ -8,5 +10,5 @@ with redirect_stdout(f):
 
 f.seek(0)
 hw = f.read(11)
-
+del f
 print(' '.join(word.capitalize() for word in hw.split()))
